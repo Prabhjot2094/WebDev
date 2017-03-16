59c0ca2e6f6cae1f3ebba686358060115e2bcd7d
Prabhjot <prabhjotsingh467@gmail.com>
2017-03-16 17:57:27 +0530

    login shifted to navbar

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,21 @@
 class DashboardController < ApplicationController
-	def index
+		def index
+		end
+		
+		def chkCredentials
+			print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+			print params[:password]
+			user = User.find_for_authentication(:email => params[:email])
+			print user
+			if user.valid_password?(params[:password])==true
+				sign_in user
+				print "---------------------------------------"
+				print user_signed_in?
+				redirect_to("/dashboard/index")
+				return
+			else
+				redirect_to("/dashboard/index")
+				return 
+			end
+		end	
 	end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  post 'dashboard/chkCredentials' => 'dashboard#chkCredentials'
+  post 'application/chkCredentials' => 'application#chkCredentials'
+  
   devise_for :users
   resources :dashboard
   root 'dashboard#index'
